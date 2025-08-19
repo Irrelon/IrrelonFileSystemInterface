7320b61cd3dea49d52f7007429d90f689ebf092c
Rob Evans <rob@irrelon.com>
2025-08-19 11:59:52 +0100

    Added missing interface

---
+++ src/Irrelon/FileSystemInterface.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+class FileSystemInterface {
+	public:
+		virtual void setup(std::string rootPath) = 0;
+		virtual bool exists(const char* path) = 0;
+		virtual bool write(const char* path, const std::vector<uint8_t>& writeData) = 0;
+		virtual bool read(const char* path, std::vector<uint8_t>& readData) = 0;
+		virtual ~FileSystemInterface() = default;
+};
