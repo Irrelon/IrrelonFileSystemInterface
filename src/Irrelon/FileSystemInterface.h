4d514c14c60741825cdc10915e9bb7f1240cef54
Rob Evans <rob@irrelon.com>
2025-08-19 12:00:42 +0100

    Added namespace

--- src/Irrelon/FileSystemInterface.h
+++ src/Irrelon/FileSystemInterface.h
@@ -4,11 +4,13 @@
 #include <string>
 #include <vector>
 
-class FileSystemInterface {
+namespace Irrelon {
+	class FileSystemInterface {
 	public:
 		virtual void setup(std::string rootPath) = 0;
 		virtual bool exists(const char* path) = 0;
 		virtual bool write(const char* path, const std::vector<uint8_t>& writeData) = 0;
 		virtual bool read(const char* path, std::vector<uint8_t>& readData) = 0;
 		virtual ~FileSystemInterface() = default;
-};
+	};
+}
